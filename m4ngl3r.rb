64ffc37c2cac19261c70f22a633f2ca6dcbddcf7
cw4d3 <cwademusic@gmail.com>
2016-07-04 01:51:53 -0700

    adding command line options

--- m4ngl3r.rb
+++ m4ngl3r.rb
@@ -4,10 +4,9 @@
 #
 # String is passed as argument, program finds all combinations of upper/lower-case, and "leet" transforms.
 
-@input = ARGV[0]
+require 'optparse'
 
 @list = []
-
 @l33t_list = {
     "a"     => ["4", "@"],
     "b"     => ["8"],
@@ -15,13 +14,94 @@
     "e"     => ["3"],
     "f"     => ["ph"],
     "i"     => ["!", "1", "l"],
+    "l"     => ["1"],
     "o"     => ["0"],
     "s"     => ["5", "$"],
     "t"     => ["7", "+"],
     "z"     => ["2"]
 }
 
-def case_permutations(str) # Credit: http://stackoverflow.com/questions/1390147/capitalization-permutations
+@options = {}
+optparse = OptionParser.new do |opts|
+	opts.banner = "Example Usage: ruby m4ngl3r.rb <-f /path/to/wordlist | -i 'foobar'> <OPTIONS>"
+	@options[:banner] = opts
+
+	@options[:file] = nil
+	opts.on('-f', '--file=FILE', String, 'Specify the path to your wordlist to mangle.') do |file|
+		@options[:file] = file
+	end
+
+    @options[:input] = nil
+	opts.on('-i', '--input=INPUT', String, 'Specify a single string as input to mangle.') do |input|
+		@options[:input] = [input]
+	end
+
+	@options[:case] = nil
+	opts.on('-c', '--case', 'Choose this option to get all case permutations.') do
+		@options[:case] = true
+	end
+
+	@options[:leet] = nil
+	opts.on('-l', '--leet', 'Choose this option to l33tify the input.') do
+		@options[:leet] = true
+	end
+
+	@options[:all] = nil
+	opts.on('-b', '--both', 'Choose this option to get all permutations (case + leet).') do
+		@options[:all] = true
+	end
+
+    @options[:prefix] = nil
+	opts.on('-p', '--prefix=PREFIX', "Add a prefix to each item. Separate with commas to make a list (-p '1,1!,123!') Note: use single quotes") do |prefix|
+		@options[:prefix] = prefix
+	end
+
+    @options[:suffix] = nil
+	opts.on('-s', '--suffix=SUFFIX', "Add a suffix to each item. Separate with commas to make a list (-s '1,1!,123!') Note: use single quotes") do |suffix|
+		@options[:suffix] = suffix
+	end
+
+	opts.on_tail('-h', '--help', "Display this screen\n\n") do
+		puts opts
+		exit
+	end
+end
+
+if !ARGV[0]
+	puts @options[:banner]
+	exit
+end
+
+optparse.parse!
+
+if @options[:file] == nil && @options[:input] == nil
+	puts "Specify a file (-f) or a string for input (-i).\n"
+	puts @options[:banner]
+	exit
+end
+
+if @options[:case] == nil && @options[:leet] == nil && @options[:all] == nil
+	puts "Mangle type is required. (-c, -l, or -b)\n"
+	puts @options[:banner]
+	exit
+end
+
+if @options[:file] != nil && @options[:input] == nil
+    @input = File.readlines(@options[:file]).each {|l| l.strip!} #remove /n chars
+elsif @options[:file] != nil && @options[:input] != nil
+    puts "You can only choose one: file as input (-f) or string as input (-i)."
+    exit
+end
+
+if @options[:input] != nil && @options[:file] == nil
+    @input = @options[:input]
+elsif @options[:file] != nil && @options[:input] != nil
+    puts "You can only choose one: file as input (-f) or string as input (-i)."
+    exit
+end
+
+def case_permutations(str)
+# Credit: http://stackoverflow.com/questions/1390147/capitalization-permutations
     perms = [""]
     tail = str.downcase
 
@@ -36,8 +116,7 @@ def case_permutations(str) # Credit: http://stackoverflow.com/questions/1390147/
             end
         end
     end
-    @list = perms
-    @list.each {|item| l33t_permutations(item)}
+    @list += perms
 end
 
 def l33t_permutations(str)
@@ -85,10 +164,45 @@ def l33t_permutations(str)
     @list += results
 end
 
-case_permutations(@input)
-
-@list.each do |item|
-    l33t_permutations(item)
+def add_prefix(prefixes)
+    pref_result = []
+    prefixes = prefixes.split(",")
+    prefixes.each do |pre|
+        items = @list.map {|x| pre + x}
+        pref_result.push items
+    end
+    @list += pref_result.flatten!
 end
 
-puts @list.uniq
+def add_suffix(suffixes)
+    suff_result = []
+    suffixes = suffixes.split(",")
+    suffixes.each do |suff|
+        items = @list.map {|x| x + suff}
+        suff_result.push items
+    end
+    @list += suff_result.flatten!
+end
+
+if @options[:case]
+	@input.each {|i| case_permutations(i)}
+end
+
+if @options[:leet]
+	@input.each {|i| l33t_permutations(i)}
+end
+
+if @options[:all]
+	@input.each {|i| case_permutations(i)}
+	@list.each {|l| l33t_permutations(l)}
+end
+
+if @options[:prefix]
+    add_prefix(@options[:prefix])
+end
+
+if @options[:suffix]
+    add_suffix(@options[:suffix])
+end
+
+puts @list
